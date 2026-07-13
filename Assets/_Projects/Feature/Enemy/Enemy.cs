054a17fe03f64a5f083b653166dec4d01db198e6
kisung71 <125112045+kisung71@users.noreply.github.com>
2026-07-13 13:53:56 +0900

    Delete CombetManager.cs

--- Assets/_Projects/Feature/Enemy/Enemy.cs
+++ Assets/_Projects/Feature/Enemy/Enemy.cs
@@ -12,24 +12,6 @@ public class Enemy : Entity
     {
 
     }
-
-    private CardData GenerateRandomCard()
-    {
-        CombatAttribute randomAttribute = (CombatAttribute)Random.Range(0, 3);
-        EffectActivateCondition randomCondition = (EffectActivateCondition)Random.Range(0, 3);
-        return new CardData(null, randomAttribute, randomCondition);
-    }
-
-    public List<CardData> GenerateRandomDeck(int count)
-    {
-        List<CardData> deck = new List<CardData>();
-
-        for (int i = 0; i < count; i++)
-        {
-            CardData card = GenerateRandomCard();
-            deck.Add(card);
-        }
-
-        return deck;
-    }
+    //CardManager·Î ÀÌÀüµÊ
+    
 }

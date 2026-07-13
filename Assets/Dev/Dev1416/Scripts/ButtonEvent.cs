fd5b89becfbd352b25b060b2a7188410f6746420
dawinpurple <dawinpurple@gmail.com>
2026-07-14 02:52:12 +0900

    OptionWindow OnOff Edit

--- Assets/Dev/Dev1416/Scripts/ButtonEvent.cs
+++ Assets/Dev/Dev1416/Scripts/ButtonEvent.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 
 public class ButtonEvent : MonoBehaviour
 {
+    private OptionOpenAndClose OptionOCMan;
     public Animator animator; //버튼에 마우스 갖다대면
     //public GameObject Option;
     bool canAnime = false;
+
+    private void Awake()
+    {
+        OptionOCMan = OptionOpenAndClose.Instance;
+    }
     public void SceneMove() //씬 메인으로 이동
     {
         Debug.Log("버튼클릭");
@@ -16,10 +22,20 @@ public class ButtonEvent : MonoBehaviour
     {
         Debug.Log("나가기클릭");
     }
-    //public void OptionActive(GameObject OPtionPopUp) // 창 열고닫기
-    //{
-    //    OPtionPopUp.SetActive(!OPtionPopUp.activeSelf);
-    //}
+    public void OptionActive() // 창 열고닫기
+    {
+        if (!OptionOCMan.isOpen)
+        {
+            Debug.Log("옵션활성화");
+            OptionOCMan.OpenOption();
+        }
+
+        else
+        {
+            Debug.Log("옵션비활성화");
+            OptionOCMan.CloseOption();
+        }
+    }
     public void CanAnime() // 시작시 애니메이션 재생 완료 후에 다은 애니메이션ㅇ ㅣ되게하기
     {
         canAnime = true;

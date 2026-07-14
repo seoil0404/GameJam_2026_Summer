274d3956f34908868830f96f70201ea6be24b1eb
seoil0404 <seoil6795@gmail.com>
2026-07-14 20:18:00 +0900

    Trail Added

---
+++ Assets/_Projects/Feature/Views/CardView/CardViewAnimationController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class CardViewAnimationController : MonoBehaviour
+{
+    public void OverCardFight()
+    {
+
+    }
+
+    public void UnderCardFight()
+    {
+
+    }
+
+    public void OverCardFightEnd()
+    {
+
+    }
+
+    public void UnderCardFightEnd()
+    {
+
+    }
+
+    public void Destroy()
+    {
+
+    }
+}

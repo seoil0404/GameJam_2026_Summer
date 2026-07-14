89740170893c826bb7f2b8a4e5d98bbf903eb45a
chickenrang0304 <yunhwarang8282@gmail.com>
2026-07-14 13:22:42 +0900

    TestCardEffect 1 - 3

---
+++ Assets/_Projects/Feature/CardSystem/CardData/CardEffect/TestCardEffect4.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class TestCardEffect4 : CardEffectBase
+{
+    public override string Name => "Multivitamin"; //¡æ«’∫Ò≈∏πŒ
+    public override void ActivateEffect(Entity owner, Entity opponent)
+    {
+    }
+}

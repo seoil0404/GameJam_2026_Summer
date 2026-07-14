fe5dcaa6e5241248740ae4769da82469bb7a3abd
dawinpurple <dawinpurple@gmail.com>
2026-07-14 23:18:00 +0900

    Ranking System Add

--- Assets/Dev/Dev1416/Scripts/ButtonEvent.cs
+++ Assets/Dev/Dev1416/Scripts/ButtonEvent.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class ButtonEvent : MonoBehaviour
 {
     private OptionOpenAndClose OptionOCMan;
+    private RankingPopUp RankingMan;
+
     public Animator animator; //버튼에 마우스 갖다대면
     //public GameObject Option;
     bool canAnime = false;
@@ -11,6 +13,13 @@ public class ButtonEvent : MonoBehaviour
     private void Start()
     {
         OptionOCMan = OptionOpenAndClose.Instance;
+        RankingMan = RankingPopUp.Instance;
+    }
+    public void MoveCharStartScene() //씬 메인으로 이동
+    {
+        Debug.Log("버튼클릭");
+
+        SceneController.LoadScene(SceneType.StartScene);
     }
     public void MoveCharSelectScene() //씬 메인으로 이동
     {
@@ -23,6 +32,12 @@ public class ButtonEvent : MonoBehaviour
         Debug.Log("버튼클릭");
 
         SceneController.LoadScene(SceneType.MainScene);
+    }
+    public void MoveSucessScene() //씬 메인으로 이동
+    {
+        Debug.Log("버튼클릭");
+
+        SceneController.LoadScene(SceneType.SucessScene);
     }
     public void GameQuit() // 게임 종료
     {
@@ -40,6 +55,20 @@ public class ButtonEvent : MonoBehaviour
         {
             Debug.Log("옵션비활성화");
             OptionOCMan.CloseOption();
+        }
+    }
+    public void RankingActive() // 창 열고닫기
+    {
+        if (!RankingMan.isActive)
+        {
+            Debug.Log("랭킹활성화");
+            RankingMan.Open();
+        }
+
+        else
+        {
+            Debug.Log("랭킹비활성화");
+            RankingMan.Close();
         }
     }
     public void CanAnime() // 시작시 애니메이션 재생 완료 후에 다은 애니메이션ㅇ ㅣ되게하기
---
+++ Assets/Dev/Dev1416/Scripts/DataSave.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class RankingData
+{
+    // 이름 없이 오직 시간 기록들만 담는 리스트
+    public List<float> rankList = new List<float>();
+}
+
+
